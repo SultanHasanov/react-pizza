55a394307d0ed648ad21e23bbc6aaacaabd123de
sultan <sultanhasanov987@gmail.com>
2022-11-04 17:39:45 +0300

    Сделал верстку корзины

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 <h1>npm i</h1>
 <h1>npm start</h1>
-<img src="./react-pizza.bmp" alt="" />
--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React from "react";
-import axios from "axios";
 import Header from "./components/Header";
 import "./scss/app.scss";
-import  Home  from "./pages/Home";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart";
 
 function App() {
-  const [pizzas, setPizzas] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  const getPizzas = async () => {
-    const res = await axios(
-      "https://63642ce67b209ece0f42316d.mockapi.io/items"
-    );
-    
-    setPizzas(res.data);
-    setIsLoading(false);
-    
-  };
-  React.useEffect(() => {
-    getPizzas();
-  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
-          {/* <NotFound /> */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
