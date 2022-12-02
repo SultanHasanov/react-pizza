b16a557b7ad45930b0222c675c069b52d8ed22f1
sultan <sultanhasanov987@gmail.com>
2022-12-02 09:59:16 +0300

    Добавил условие не больше 5 пицц

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 
 import { addItem } from '../../features/cartSlice'
@@ -10,8 +10,12 @@ const PizzaBlock = ({ id, title, price, imageUrl, sizes, types }) => {
   const dispatch = useDispatch()
   const cartItem = useSelector(state => state.cart.items.find(obj => obj.id === id))
   const addedCount = cartItem ? cartItem.count : 0
+  const [text, setText] = useState('Хватит')
 
   const onClickAdd = () => {
+    if(addedCount >= 5){
+      setText(text)
+    }
     const item = {
       id,
       title,
@@ -60,10 +64,10 @@ const PizzaBlock = ({ id, title, price, imageUrl, sizes, types }) => {
           </div>
           <div className="pizza-block__bottom">
             <div className="pizza-block__price">от {price} ₽</div>
-            <button
+            <button disabled={addedCount >= 5}
               onClick={onClickAdd}
               className="button button--outline button--add"
-            >
+              >
               <svg
                 width="12"
                 height="12"
@@ -74,12 +78,13 @@ const PizzaBlock = ({ id, title, price, imageUrl, sizes, types }) => {
                 <path
                   d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                   fill="white"
-                />
+                  />
               </svg>
               <span>Добавить</span>
               {addedCount > 0 && <i>{addedCount}</i>}
             </button>
           </div>
+                  {addedCount >= 5 ? text : ''}
         </div>
       </div>
     );
