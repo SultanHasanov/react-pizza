93b905905bcc6a6431291690bf90f6f91ad10126
sultan <sultanhasanov987@gmail.com>
2023-02-19 21:06:13 +0300

    Доработал тотал в корзине

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -35,6 +35,9 @@ const cartSlice = createSlice({
     },
     removeItem(state, action) {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
     },
     clearItem(state) {
       state.items = [];
