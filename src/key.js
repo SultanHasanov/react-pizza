d6ca42b31f664e986f89bb5c6d1d901828dd46f9
sultan <sultanhasanov987@gmail.com>
2023-06-01 18:58:11 +0300

    изменил токен

--- src/key.js
+++ src/key.js
@@ -1,2 +1,2 @@
-export const token = "5827530379:AAGEcp1lsyTruMM6g5RcEQoaffMVkx3U1Mo";
+export const token = "6199714726:AAEX2ajO1qyM4E8_ShQ9cMNJvn12HPFcZrg";
 export const chat_id = "-1001811930704";
